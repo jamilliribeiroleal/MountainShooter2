6783721c3d989d87c045628388d8389bcddf539f
Jamilli <jamillileal8@gmail.com>
2025-02-23 09:52:51 -0300

    initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window = pygame.display.set_mode(size=(600, 480))
 print('Setup End')
 
 print('Loop Start')
-while True:
+while True: #loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
